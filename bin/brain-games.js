27402077938cc30c8310fdf74eeb370d6ac5a57f
b0x009dev <b0x009dev@gmail.com>
2026-03-11 16:04:26 +0300

    feat: add welcome and username prompt

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
+import { sayHello } from '../src/cli.js';
+
 const printGreeting = () => {
     console.log('Welcome to the Brain Games!');
 };
 
 printGreeting();
+sayHello();
