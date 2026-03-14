a996be8999c46727cd6e2206441e4df330e1a0b2
b0x009dev <b0x009dev@gmail.com>
2026-03-14 14:56:25 +0300

    refactor: move game registry to a separate module

--- src/pipeline.js
+++ src/pipeline.js
@@ -1,47 +1,9 @@
 import { input } from './cli.js'
+import { getGame } from './game-registry.js'
 import { greeting } from './greeting.js'
 import { gameOver } from './game-over.js'
 
-import { brainEvenRules, brainEvenRound } from './games/brain-even-implementation.js'
-import { brainCalcRules, brainCalcRound } from './games/brain-calc-implementation.js'
-import { brainGCDRules, brainGCDRound } from './games/brain-gcd-implementation.js'
-import { brainProgressionRules, brainProgressionRound } from './games/brain-progression-implementation.js'
-import { brainPrimeRules, brainPrimeRound } from './games/brain-prime-implementation.js'
-
-const gameRegistry = {
-  'brain-even': {
-    gameRules: brainEvenRules,
-    gameRound: brainEvenRound,
-  },
-  'brain-calc': {
-    gameRules: brainCalcRules,
-    gameRound: brainCalcRound,
-  },
-  'brain-gcd': {
-    gameRules: brainGCDRules,
-    gameRound: brainGCDRound,
-  },
-  'brain-progression': {
-    gameRules: brainProgressionRules,
-    gameRound: brainProgressionRound,
-  },
-  'brain-prime': {
-    gameRules: brainPrimeRules,
-    gameRound: brainPrimeRound,
-  },
-}
-
 const GAME_ROUNDS = 3
-
-const getGame = (gameName) => {
-  const game = gameRegistry[gameName]
-
-  if (!game) {
-    throw new Error(`Unknown game: ${gameName}`)
-  }
-
-  return game
-}
 
 export const pipeline = (gameName) => {
   const userName = greeting()
